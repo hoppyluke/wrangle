8e9fd89dbf939e9da3c8fcef54e67feb9d7f1091
Luke <luke.hopton@gmail.com>
2016-01-21 23:00:05 +0000

    Add basic parsing for custom object

---
+++ Wrangle/Wrangle/Instance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wrangle
+{
+    public static class Instance<T> where T : new()
+    {
+        public static T From(string[] args)
+        {
+            var value = new T();
+
+            if (args == null || args.Length == 0) return value;
+
+            var properties = typeof(T).GetProperties()
+                .Where(p => p.GetGetMethod() != null && p.GetSetMethod() != null);
+
+            var d = Wrangle.Dictionary.From(args);
+
+            foreach(var pair in d)
+            {
+                var property = properties.Where(p => p.Name == pair.Key).Single();
+                property.SetValue(value, pair.Value);
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Wrangle/WrangleTests/Instance/FromTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WrangleTests.Instance
+{
+    [TestFixture]
+    public class FromTests
+    {
+        [Test]
+        public void ShouldAcceptNullArgs()
+        {
+            string[] a = null;
+            var args = Wrangle.Instance<MyArgs>.From(a);
+            
+            Assert.That(args, Is.Not.Null);
+            AssertIsDefault(args);
+        }
+
+        [Test]
+        public void ShouldAcceptEmptyArgs()
+        {
+            var a = new string[0];
+            var args = Wrangle.Instance<MyArgs>.From(a);
+
+            Assert.That(args, Is.Not.Null);
+            AssertIsDefault(args);
+        }
+
+        [Test]
+        public void ShouldSetStringValues()
+        {
+            var a = new[] { "Id", "my-id", "Name", "my-name" };
+            var args = Wrangle.Instance<MyStringArgs>.From(a);
+
+            Assert.That(args, Is.Not.Null);
+            Assert.That(args.Id, Is.EqualTo("my-id"));
+            Assert.That(args.Name, Is.EqualTo("my-name"));
+        }
+
+        private static void AssertIsDefault(object o)
+        {
+            var properties = from p in o.GetType().GetProperties()
+                             where p.GetSetMethod() != null
+                             && p.GetSetMethod() != null
+                             select p;
+
+            foreach(var p in properties)
+            {
+                var v = p.GetValue(o);
+                if (p.PropertyType.IsValueType)
+                    Assert.That(v, Is.EqualTo(Activator.CreateInstance(p.PropertyType)));
+                else
+                    Assert.That(v, Is.Null);
+            }
+        }
+        
+        private class MyArgs
+        {
+            public string Name { get; set; }
+
+            public int Value { get; set; }
+        }
+
+        private class MyStringArgs
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
